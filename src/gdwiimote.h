b4e1ed5296bde5c4307d94386df297f7799610f9
Adrian Edwards <ace4971@rit.edu>
2023-11-08 13:00:20 -0500

    Convert class back to a Node

--- src/gdwiimote.h
+++ src/gdwiimote.h
@@ -1,13 +1,13 @@
 #ifndef GDWIIMOTE_H
 #define GDWIIMOTE_H
 
-#include <godot_cpp/classes/ref.hpp>
+#include <godot_cpp/classes/node3d.hpp>
 #include "wiiuse.h"                     /* for wiimote_t, classic_ctrl_t, etc */
 
 namespace godot {
 
-class GDWiimote : public RefCounted {
-	GDCLASS(GDWiimote, RefCounted)
+class GDWiimote : public Node3D {
+	GDCLASS(GDWiimote, Node3D)
 
 private:
 	wiimote** wiimotes;
