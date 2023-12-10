33f58388411aeaaa801bbc901e14eb4788ca5428
Adrian Edwards <ace4971@rit.edu>
2023-12-09 21:13:00 -0500

    add scene changes to the other menu buttons

--- demo/Menu.gd
+++ demo/Menu.gd
@@ -1,16 +1,11 @@
 extends Node2D
 
 
-
 func _on_targets_pressed():
-	pass # Replace with function body.
-
-
-
+	get_tree().change_scene_to_file("res://targets.tscn")
 
 func _on_stadium_pressed():
-	pass # Replace with function body.
-
+	get_tree().change_scene_to_file("res://picture.tscn")
 
 func _on_quit_pressed():
 	get_tree().quit()
