2768595fbc3dee7431830d13a50ce9383078ccd0
Adrian Edwards <ace4971@rit.edu>
2023-12-10 12:10:51 -0500

    adjust sensitivity for targets demo

--- demo/mote.gd
+++ demo/mote.gd
@@ -7,9 +7,9 @@ func _ready():
 	#gdwiimote = GDWiimote.new()
 	print("connecting....")
 	num_connected = wiimote_connect(5)
-	set_x_sensitivity(0.5)
-	set_y_sensitivity(0.5)
-	set_z_sensitivity(0.5)
+	set_x_sensitivity(0.05)
+	set_y_sensitivity(0.05)
+	set_z_sensitivity(0.2)
 	print("turn on your wiimote now....")
 	# pass # Replace with function body.
 	if num_connected == 0:
