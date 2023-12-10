e31055d888e97ff37fa4938dfe9c75a3e97a4337
Adrian Edwards <ace4971@rit.edu>
2023-12-09 21:08:53 -0500

    menu scene with buttons and working quit

---
+++ demo/Menu.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+
+func _on_targets_pressed():
+	pass # Replace with function body.
+
+
+
+
+func _on_stadium_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_pressed():
+	get_tree().quit()
