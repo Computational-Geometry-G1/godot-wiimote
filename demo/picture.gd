6a10283f809f3312c3b50dcba577fefa511ce98a
Adrian Edwards <ace4971@rit.edu>
2023-12-10 10:44:02 -0500

    use mouse data as a fallback for the picture demo

--- demo/picture.gd
+++ demo/picture.gd
@@ -18,26 +18,24 @@ func _ready():
 	# pass # Replace with function body.
 	if num_connected == 0:
 		print("none connected - falling back to mouse data")
+		Input.set_mouse_mode(2)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if num_connected > 0:
 		poll()
-	else:
-		simulate_data(delta)
+	
 		
-	print(delta)
-	print(get_position())
-	print()
+		print(delta)
+		print(get_position())
+		print()
 
-
-
-func _ready() -> void:
-	Input.set_mouse_mode(2)
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion && Input.get_mouse_mode() == 2:
-		_camera.rotate_x(deg_to_rad(-event.relative.y * 0.1))
-		rotate_y(deg_to_rad(-event.relative.x * 0.1))
-		_camera.rotation_degrees.x = clamp(_camera.rotation_degrees.x,-89.9,89.9)
+	if num_connected == 0:
+		if event is InputEventMouseMotion && Input.get_mouse_mode() == 2:
+			_camera.rotate_x(deg_to_rad(-event.relative.y * 0.1))
+			rotate_y(deg_to_rad(-event.relative.x * 0.1))
+			_camera.rotation_degrees.x = clamp(_camera.rotation_degrees.x,-89.9,89.9)
 	
